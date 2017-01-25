cd677ad40c4a3b7edd5dae0d1c65951f4da5d69b
TreeNewBee321 <751564354@qq.com>
2017-01-25 18:08:21 +0800

    修改首页

--- 10web/application/views/welcome.php
+++ 10web/application/views/welcome.php
@@ -1,246 +1,183 @@
 <!DOCTYPE html>
-<html>
-<head lang="en">
+<!-- saved from url=(0049)file:///C:/Users/fancunhao/Desktop/10/index.html# -->
+<html class="js cssanimations"><head lang="en"><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
   <meta charset="UTF-8">
   <title>亿灵软件开发联盟</title>
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport"
-        content="width=device-width, initial-scale=1">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="format-detection" content="telephone=no">
   <meta name="renderer" content="webkit">
-  <meta http-equiv="Cache-Control" content="no-siteapp"/>
-  <link rel="alternate icon" type="image/png" href="#">
-  <link rel="stylesheet" href="assets/css/app.css"/>
-  <style>
-    .get {
-      background: #1f7dba;
-      color: #fff;
-      text-align: center;
-      padding: 200px 0;
-    }
+  <meta http-equiv="Cache-Control" content="no-siteapp">
+  <link rel="alternate icon" type="image/png" href="assets/i/favicon.png">
+  <link rel="stylesheet" href="assets/css/app.css">
+  <link rel="stylesheet" href="assets/css/amazeui.min.css">
 
-    .get-title {
-      font-size: 200%;
-      border: 2px solid #fff;
-      padding: 20px;
-      display: inline-block;
-    }
-
-    .get-btn {
-      background: #fff;
-    }
-
-    .detail {
-      background: #fff;
-    }
-
-    .detail-h2 {
-      text-align: center;
-      font-size: 150%;
-      margin: 40px 0;
-    }
-
-    .detail-h3 {
-      color: #1f8dd6;
-    }
-
-    .detail-p {
-      color: #7f8c8d;
-    }
-
-    .detail-mb {
-      margin-bottom: 30px;
-    }
-
-    .hope {
-      background: #3399FF;
-      padding: 50px 0;
-    }
-
-    .hope-img {
-      text-align: center;
-    }
-
-    .hope-hr {
-      border-color: #149C88;
-    }
-
-    .hope-title {
-      font-size: 140%;
-    }
-
-    .footer p {
-      color: #7f8c8d;
-      margin: 0;
-      padding: 15px 0;
-      text-align: center;
-      background: #2d3e50;
-    }
-  </style>
 </head>
-<body>
-<header class="am-topbar am-topbar-fixed-top">
-  <div class="am-container">
-    <h1 class="am-topbar-brand">
-      <a href="#">亿灵软件开发联盟</a>
-    </h1>
-
-    <button class="am-topbar-btn am-topbar-toggle am-btn am-btn-sm am-btn-secondary am-show-sm-only"
-            data-am-collapse="{target: '#collapse-head'}"><span class="am-sr-only">导航切换</span> <span
-        class="am-icon-bars"></span></button>
-
-    <div class="am-collapse am-topbar-collapse" id="collapse-head">
-      <ul class="am-nav am-nav-pills am-topbar-nav">
-        <li class="am-active"><a href="#">首页</a></li>
-        <li><a href="#">项目</a></li>
-        <li class="am-dropdown" data-am-dropdown>
-          <a class="am-dropdown-toggle" data-am-dropdown-toggle href="javascript:;">
-          <span class="am-icon-caret-down">导航栏</span>
-          </a>
-          <ul class="am-dropdown-content">
-            <li class="am-dropdown-header">标题</li>
-            <li><a href="#">1. 社团简介</a></li>
-            <li><a href="#">2. 活动内容</a></li>
-            <li><a href="#">3. 生活服务</a></li>
-            <li><a href="#">4. 技术文章</a></li>
+<body class="am-with-topbar-fixed-top">
+<header class="am-topbar am-topbar-fixed-top am-topbar-white">
+  <div class="am-header-border ">                </div>
+  <div class="am-container am-topbar-height">
+    <div class="am-topbar-brand am-brand">
+      <div style="float:left;">
+      <img src="assets/i/examples/10logo1.png" style="height:60px; margin:0 1%;" >
+      </div>
+      <div class="am-span">
+      <a class="am-dropdown-toggle"  href="javascript:;">
+      <span class="dis">首页</span></a>
+      </div>
+      <div class="am-span">
+      <a class="am-dropdown-toggle"  href="javascript:;">
+      <span class="dis">项目</span></a>
+      </div>
+      <div class="am-span ">
+      <a class="am-dropdown-toggle" data-am-dropdown-toggle="" href="javascript:;">
+      <li class="am-dropdown am-navvv" data-am-dropdown="">
+      <span class="dis ">导航栏</span></a>
+      </a>
+          <ul class="am-dropdown-content am-content">
+            <li class=""></li>
+            <li><a href="">社团简介</a></li>
+            <li><a href="">技术文章</a></li>
+            <li><a href="">社团活动</a></li>
+            <li><a href="">生活服务</a></li>
           </ul>
-        </li>
-      </ul>
-      
-      <div class="am-topbar-right">
-        <button class="am-btn am-btn-secondary am-topbar-btn am-btn-sm"><span class="am-icon-pencil"></span> 注册</button>
+      </li>
       </div>
 
-      <div class="am-topbar-right">
-        <button class="am-btn am-btn-primary am-topbar-btn am-btn-sm"><span class="am-icon-user"></span> 登录</button>
+       
+      <div class="am-topright am-topbar-right am-topright" style="float:right;">
+       <a class="am-dropdown-toggle" data-am-dropdown-toggle="" href="javascript:;">
+        <span class="am-icon-pencil "></span> 注册</button>
+       </a> 
       </div>
+
+      <div class="am-topright am-topbar-right " style="float:right;">
+       <a class="am-dropdown-toggle" data-am-dropdown-toggle="" href="javascript:;">
+        <span class="am-icon-user "></span> 登录</button>
+      </a>
+      </div>
+    </div>      
     </div>
   </div>
 </header>
 
-<div class="get">
-  <div class="am-g">
-    <div class="am-u-lg-12">
-      <h1 class="get-title">最新活动就放在这块蓝色的下面</h1>
 
-      <p>
-        此处应有图片
-      </p>
-
-      <p>
-        <a href="http://amazeui.org" class="am-btn am-btn-sm get-btn">点击图片登录√</a>
-      </p>
-    </div>
+<div id="am-wrapper">
+  <div id="am-banner">
+    <ul id="am-imglist">
+    <li><img src="./index_files/gungun.png" class="am-banner"></li>
+    <li><img src="./index_files/gungun.png" class="am-banner"></li>
+    <li><img src="./index_files/gungun.png" class="am-banner"></li>
+  </div>
+  <div class="am-container">
+    <div ><a class="am-icon-chevron-circle-right am-icon-lg" id="next" href=""></a></div>
+    <div ><a class="am-icon-chevron-circle-left am-icon-lg" id="prev" href=""></a></div>
+    
   </div>
 </div>
 
 
-<div class="detail">
-  <div class="am-g am-container">
-    <div class="am-u-lg-12">
-      <h2 class="detail-h2"></h2>
 
-      <div class="am-g">
-        <div class="am-u-lg-3 am-u-md-6 am-u-sm-12 detail-mb">
+<div class="detail am-container">
+    <div class="intro">
 
-          <h3 class="detail-h3">            
-            <i class="am-icon-mobile am-icon-sm"></i>
-            社团简介
-          </h3>
+      <div class="am-center am-icon-book am-icon-lg am-wrap1"></div>
+      <div class="intro am-wrap2">社团简介
+        
+        <p class="am-fontsize">我们是一个什么样的社团嗯？
+        </p>
 
-          <p class="detail-p">
-            大声告诉它们亿灵是做什么的！
-            大声告诉它们亿灵是做什么的！
-            大声告诉它们亿灵是做什么的！
-            大声告诉它们亿灵是做什么的！
-            大声告诉它们亿灵是做什么的！
-            大声告诉它们亿灵是做什么的！
-          </p>
-          <a href="/index.php/team/detail">详情>></a>
+      </div>
 
-        </div>
-        <div class="am-u-lg-3 am-u-md-6 am-u-sm-12 detail-mb">
-          <h3 class="detail-h3">
-            <i class="am-icon-cogs am-icon-sm"></i>
-            技术文章
-          </h3>
+    </div>
+    <div class="article">
 
-          <p class="detail-p">
-            大佬们写出来给我们看的文章
-            大佬们写出来给我们看的文章
-            大佬们写出来给我们看的文章
-            大佬们写出来给我们看的文章
-            大佬们写出来给我们看的文章
-            大佬们写出来给我们看的文章
-          </p>
-          <a href="www.baidu.com">详情>></a>
-        </div>
-        <div class="am-u-lg-3 am-u-md-6 am-u-sm-12 detail-mb">
-          <h3 class="detail-h3">
-            <i class="am-icon-check-square-o am-icon-sm"></i>
-            活动内容
-          </h3>
+      <div class="am-center am-icon-gear am-icon-lg am-wrap1">
+      </div>
+      <div class="article am-wrap2">技术文章
+        <p class="am-fontsize">让我们遨游在技术的海洋里！</p>
+      </div>
 
-          <p class="detail-p">
-            带你体验VR！
-            带你体验VR！
-            带你体验VR！
-            带你体验VR！
-            带你体验VR！
-            带你体验VR！
-            带你体验VR！
-            带你体验VR！
-            带你体验VR！
-            带你体验VR！
-            带你体验VR！
-            带你体验VR！
-            带你体验VR！
-            带你体验VR！
-          </p>
-          <a href="www.baidu.com">详情>></a>
-        </div>
-        <div class="am-u-lg-3 am-u-md-6 am-u-sm-12 detail-mb">
-          <h3 class="detail-h3">
-            <i class="am-icon-send-o am-icon-sm"></i>
-            生活服务
-          </h3>
+    </div>    
+    <div class="activity">
 
-          <p class="detail-p">
-            查天气查公交，应有尽有！
-            查天气查公交，应有尽有！
-            查天气查公交，应有尽有！
-            查天气查公交，应有尽有！
-            查天气查公交，应有尽有！
-            查天气查公交，应有尽有！
-            查天气查公交，应有尽有！
-          </p>
-          <a href="www.baidu.com">详情>></a>
-        </div>
+      <div class="am-center am-icon-motorcycle am-icon-lg am-wrap1"></div>
+      <div class="activity am-wrap2">社团活动
+        <p class="am-fontsize">一起来看看社团最新的活动！</p>
+      </div>
+ 
+    </div>
+    <div class="service ">  
+      <div class="am-center am-icon-plane am-icon-lg am-wrap1"></div>
+      <div class="service am-wrap2">生活服务
+        <p class="am-fontsize">查天气，查公交，应有尽有！</p>
       </div>
     </div>
+</div>
+<hr class=" am-container am-article-divider">
+
+
+<div class="am-divide am-container">
+  <div>-内容精选-</div>
+</div>
+
+<div class="am-summary am-container">
+  <div class="am-summary1">
+    <div class="am-summary-pic"><img src="assets/i/examples/p1153075313.jpg " class="am-summary-pic"></div>
+    <h2 class="am-summary-title">Javascript全接触</h2>
+    <p class="am-summary-content am-wrap2">现今Javascript已经成为网络开发中不可或缺的一部分</p>
   </div>
+    <div class="am-summary1">
+    <div class="am-summary-pic"><img src="assets/i/examples/p1153075313.jpg " class="am-summary-pic"></div>
+    <h2 class="am-summary-title">Javascript全接触</h2>
+    <p class="am-summary-content">现今Javascript已经成为网络开发中不可或缺的一部分</p>
+  </div>
+    <div class="am-summary1">
+    <div class="am-summary-pic"><img src="assets/i/examples/p1153075313.jpg " class="am-summary-pic"></div>
+    <h2 class="am-summary-title">Javascript全接触</h2>
+    <p class="am-summary-content">现今Javascript已经成为网络开发中不可或缺的一部分</p>
+  </div>
+    <div class="am-summary1">
+    <div class="am-summary-pic"><img src="assets/i/examples/p1153075313.jpg " class="am-summary-pic"></div>
+    <h2 class="am-summary-title">Javascript全接触</h2>
+    <p class="am-summary-content">现今Javascript已经成为网络开发中不可或缺的一部分</p>
+  </div>
+</div>
+<div>
+  <div>
+  </div>
+</div>
+<hr class=" am-container am-article-divider">
+<div class="am-drop">
+<a href=""><span class="am-icon-chevron-circle-down am-icon-lg am-drop"></span></a>
 </div>
 
 <div class="hope">
   <div class="am-g am-container">
-    <div class="am-u-lg-4 am-u-md-6 am-u-sm-12 hope-img">
-      <img src="assets/i/examples/landing.png" alt="" data-am-scrollspy="{animation:'slide-left', repeat: false}">
-      <hr class="am-article-divider am-show-sm-only hope-hr">
+    <div class="am-leftpart">
+        <div><h1 class="hope-title hope-font ">与亿灵一起</h1></div>
+        <div><p class="hope-font hope-content">永远不成为技术的过客，永远在路上</p></div>
     </div>
-    <div class="am-u-lg-8 am-u-md-6 am-u-sm-12">
-      <h2 class="hope-title">与亿灵一起畅游在10的海洋</h2>
-
-      <p>
-        永远不成为技术的过客，永远在路上。
-      </p>
+    <div class="am-rightpart">
+        <div><h1 class="hope-title hope-font">友情链接</h1></div>
+        <div >
+          <p class="hope-font ">
+            <a class="am-dropdown-toggle hope-font hope-content" data-am-dropdown-toggle="" href="javascript:;">南开大学</a>
+          </p>
+          <p class="hope-font ">
+            <a class="am-dropdown-toggle hope-font hope-content" data-am-dropdown-toggle="" href="javascript:;">南开大学软件学院</a>
+          </p>
+          <p class="hope-font ">
+            <a class="am-dropdown-toggle hope-font hope-content" data-am-dropdown-toggle="" href="javascript:;">CI框架</a>
+          </p>
+          <p class="hope-font ">
+            <a class="am-dropdown-toggle hope-font hope-content" data-am-dropdown-toggle="" href="javascript:;">南开腾讯俱乐部</a>
+          </p>
     </div>
   </div>
 </div>
 
-<footer class="footer">
-  <p>友情链接       <a href="http://www.yunshipei.com" target="_blank">亿灵首页|</a>南开主页|<a
-      href="http://opensource.org/licenses/MIT" target="_blank">教育在线|</a>南开大学软件学院</p>
-</footer>
+
 
 <!--[if lt IE 9]>
 <script src="http://libs.baidu.com/jquery/1.11.1/jquery.min.js"></script>
@@ -249,8 +186,11 @@
 <![endif]-->
 
 <!--[if (gte IE 9)|!(IE)]><!-->
-<script src="http://cdn.bootcss.com/jquery/3.1.1/jquery.min.js"></script>
+<script src="./index_files/jquery-3.1.1.min.js"></script>
+<script src="assets/js/jquery.min.js"></script>
 <!--<![endif]-->
 <script src="assets/js/amazeui.min.js"></script>
-</body>
-</html>
+<script src="assets/js/app.js"></script>
+<script type="text/javascript">
+</script>
+</body></html>
