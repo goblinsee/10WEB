8f0da40d9abc9e8299ba0c5b81a7883589298b48
huangjingping <1304272317@qq.com>
2017-02-12 09:40:16 +0800

    分离出Message.php以及修改id生成方式

--- 10web/application/controllers/api/Admin.php
+++ 10web/application/controllers/api/Admin.php
@@ -23,33 +23,14 @@ class Admin extends CI_Controller {
       return array("Flag" => $Flag,"Content" => $Content,"Extra" => $Extra);
     }
 
-    /**
-    *   查看所有用户的消息
-    */
-    public function GetAllUsersMessages(){
-        $messages = $this->usermessage_model->GetAllUsersMessages();
-        print_r($messages);
-    }
+    
 
     /**
     *   给用户发消息,如果有用户id传入就给该用户发送消息，否则给所有用户发送消息
     */
    
 
-    /**
-    *   管理员删除消息
-    */
-    public function DeleteMessageForAdmin(){
-        $messageid = $this->input->post('MessageID');
-        $info = null;
-        if($this->usermessage_model->DeleteMessageForAdmin($messageid) <> 0){
-            $info = $this->getInfo(100,"delete message successful","");
-        }
-        else{
-            $info = $this->getInfo(-13,"delete message fail","");
-        }
-        echo urldecode(json_encode($info));
-    }
+    
 }
 
 ?>
