d3e93ed38a6630df362aa15b7da10534d315d41f
王理治 <liz@lizs-mbp.local>
2017-02-16 10:37:38 +0800

    添加了404网页

--- 10web/application/views/errors/html/error_404.php
+++ 10web/application/views/errors/html/error_404.php
@@ -2,10 +2,22 @@
     require('./application/views/template/html_begin.php');
     require('./application/views/template/header.php');
 ?>
-	<div id="container">
-		404 not found
-	</div>
-
+<link rel="stylesheet" type="text/css" href="/assets/css/error_404_detail.css">
+<div class="e0-error-block">
+</div>
+<div class="e0-error-background">
+	<body onload="show()">
+	<img id="showimg" width="100%" height="100%" alt="404">
+</div>
 <?php  
     require('./application/views/template/html_end.php');
 ?>
+
+<script language="javascript">
+function show()
+{
+	var NowFrame=parseInt(Math.floor(Math.random()*3) + 1);
+	document.getElementById("showimg").src="/assets/i/error_"+NowFrame+".png";
+	console.log(NowFrame);
+}
+</script>
